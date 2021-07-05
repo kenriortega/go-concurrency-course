00f3b40e013f443ea850d220d198cac723ddece4
kenriortega <kenriortega@gmail.com>
2021-07-05 18:58:49 -0400

    module pipeline completed

---
+++ pipeline/eje2/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func generator(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	// impl fan in
+	// merge a list to a single channel
+	out := make(chan int)
+	var wg sync.WaitGroup
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go func() {
+			go output(c)
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	in := generator(2, 3)
+
+	ch1 := square(in)
+	ch2 := square(in)
+
+	for n := range merge(ch1, ch2) {
+		fmt.Println(n)
+	}
+}
