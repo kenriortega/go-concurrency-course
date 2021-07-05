a3f8b3be062494a8ebf759d249af51d081057d10
kenriortega <kenriortega@gmail.com>
2021-07-05 17:32:51 -0400

    module race detect completed

---
+++ racedect/eje1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	var t *time.Timer
+	ch := make(chan bool)
+	t = time.AfterFunc(randomDuration(), func() {
+		fmt.Println(time.Since(start))
+		ch <- true
+	})
+	for time.Since(start) < 5*time.Second {
+		<-ch
+		t.Reset(randomDuration())
+	}
+	time.Sleep(5 * time.Second)
+}
+func randomDuration() time.Duration {
+	return time.Duration(rand.Int63n(1e9))
+}
